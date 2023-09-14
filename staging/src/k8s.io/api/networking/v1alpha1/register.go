222410e74846d8911b93d50cfd15632d78880083
Syed Nisar Ul Haq <nisarul@gmail.com>
2023-09-14 18:30:11 +0000

    registering new resources

--- staging/src/k8s.io/api/networking/v1alpha1/register.go
+++ staging/src/k8s.io/api/networking/v1alpha1/register.go
@@ -56,6 +56,10 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ClusterCIDRList{},
 		&IPAddress{},
 		&IPAddressList{},
+		&PodNetwork{},
+		&PodNetworkList{},
+		&PodNetworkAttachment{},
+		&PodNetworkAttachmentList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
